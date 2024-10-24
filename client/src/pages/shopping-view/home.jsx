edd842e6a14f0d70cfa1b364f56cc32d382bb5a6
Aman Kumar Singh <singhkumaraman17@gmail.com>
2024-10-24 22:23:28 +0530

    Update home.jsx

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -95,6 +95,11 @@ function ShoppingHome() {
           </div>
         </div>
       </section>
+      <section className="py-12">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-8">Feature Product </h2>
+        </div>
+      </section>
     </div>
   );
 }
